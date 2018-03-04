8565986f10a99ee303f9147bd7ab6cf048863d97
Ozbej Golob <ozbej.golob@gmail.com>
2018-03-04 22:27:38 +0100

    Comments
    Adding comments to posts is enabled, but it still has some bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304182729) do
+ActiveRecord::Schema.define(version: 20180304205019) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
