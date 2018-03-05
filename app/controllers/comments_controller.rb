0a0e6dd4801134885dca2dbb8a1a0e1504032e38
Ozbej Golob <ozbej.golob@gmail.com>
2018-03-05 10:01:55 +0100

    Working comments
    Comments are fully functional

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,28 @@
 class CommentsController < ApplicationController
 
+	def new
+    	@comment = current_user.comments.build
+  	end
+
 	def create
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(params[:comment].permit(:body))
-		@comment.user_id = current_user.id
+		@comment = current_user.comments.build(comment_params)
+		@post = Post.find(params[:post_id])	
+		@comment.post_id = @post.id
+		@comment.save
 		redirect_to post_path(@post)
 	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
+	end
+
+
+	private
+	def comment_params
+      params.require(:comment).permit(:body, :user_id, :post_id)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
+
+  has_many :comments, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
